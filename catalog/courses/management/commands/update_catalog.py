f4032dbbc6486d7375fc3190b2103a5031bfd5d4
Grisha Temchenko <soid.exe@gmail.com>
2021-05-14 13:07:23 -0400

    support importing different semesters

--- catalog/courses/management/commands/update_catalog.py
+++ catalog/courses/management/commands/update_catalog.py
@@ -12,39 +12,42 @@ class Command(BaseCommand):
         parser.add_argument('semesters', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        # TODO run for all semesters
-        year = 2021
-        semester = "Spring"
+        for term in options['semesters']:
+            tmp = term.split('-')
+            year = int(tmp[0])
+            semester = tmp[1]
+            print("Running for", term)
 
-        with open("/columbia-catalog-data/classes/2021-Spring.json") as fclasses:
-            num = 1
-            for line in fclasses:
-                course = json.loads(line)
-                # find existing one
-                obj = Course.objects.filter(year=year,
-                                      semester=semester,
-                                      call_number=course['call_number'])
-                if len(obj) > 0:
-                    obj = obj[0]
-                else:
-                    # create if doesn't exist
-                    obj = Course(year=year,
-                                 semester=semester,
-                                 call_number=course['call_number'])
+            with open("/columbia-catalog-data/classes/2021-Spring.json") as fclasses:
+                num = 1
+                for line in fclasses:
+                    course = json.loads(line)
+                    # find existing one
+                    obj = Course.objects.filter(year=year,
+                                          semester=semester,
+                                          call_number=course['call_number'])
+                    if len(obj) > 0:
+                        obj = obj[0]
+                    else:
+                        # create if doesn't exist
+                        obj = Course(year=year,
+                                     semester=semester,
+                                     call_number=course['call_number'])
 
-                # update fields
-                for key in course.keys():
-                    if hasattr(obj, key):
-                        setattr(obj, key, course[key])
+                    # update fields
+                    for key in course.keys():
+                        if hasattr(obj, key):
+                            setattr(obj, key, course[key])
 
-                try:
-                    obj.save()
-                except (DataError, OperationalError):
-                    print("Error while processing:", course)
-                    raise
+                    try:
+                        obj.save()
+                    except (DataError, OperationalError):
+                        print("Error while processing:", course)
+                        raise
 
-                if num % 419 == 0:
-                    print("Processed:", num)
-                num += 1
+                    if num % 419 == 0:
+                        print("Processed:", num)
+                    num += 1
+            print("Finished for", num, "classes.")
         print('Done.')
 
