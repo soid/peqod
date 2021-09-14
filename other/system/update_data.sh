b2317c4c4111fbd65c5b8500d4ba6b1560b42de7
Grisha Temchenko <soid.exe@gmail.com>
2021-09-13 23:43:27 -0400

    load env in catalog update

--- other/system/update_data.sh
+++ other/system/update_data.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 git -C $HOME/peqod-project/columbia-catalog-data/ pull
 
+. $SCRIPT_DIR/env.sh
 . $HOME/peqod-project/venv/bin/activate
 
 python $HOME/peqod-project/peqod/catalog/manage.py update_catalog ALL
