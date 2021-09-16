1964ed4d3648c62e2585b1d96461a2facc96d595
Grisha Temchenko <soid.exe@gmail.com>
2021-09-15 23:21:04 -0400

    added gitignore

--- other/system/run-uwsgi.sh
+++ other/system/run-uwsgi.sh
@@ -7,4 +7,4 @@ source $SCRIPT_DIR/env.sh
 
 . $HOME/peqod-project/venv/bin/activate
 
-uwsgi --socket /tmp/peqod.sock --chmod-socket=666 --python-path $HOME/peqod-project/peqod/catalog --wsgi-file $HOME/peqod-project/peqod/catalog/catalog/wsgi.py
+uwsgi --die-on-term --socket /tmp/peqod.sock --chmod-socket=666 --python-path $HOME/peqod-project/peqod/catalog --wsgi-file $HOME/peqod-project/peqod/catalog/catalog/wsgi.py
